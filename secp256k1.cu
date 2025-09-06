12e2c9c8fa35e6c85132cd0ae9fc399c0d24a176
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-09-06 09:31:55 -0300

    secp256k1.cu

--- secp256k1.cu
+++ secp256k1.cu
@@ -373,7 +373,7 @@ __device__ void mod_inverse_p(uint64_t *result, const uint64_t *a_normal) {
         #pragma unroll 4
         for (int j = 0; j < 4; ++j) {
             uint64_t v_odd = v[0] & 1ULL;
-            int32_t swap_flag = ((delta > 0) ? v_odd : !v_odd);
+            int32_t swap_flag = ((delta > 0 && v_odd) || (delta < 0 && !v_odd)) ? 1 : 0;
             int64_t m = - (int64_t)swap_flag;
             uint64_t mask = (uint64_t)m;
             uint64_t inv_mask = ~mask;
@@ -396,8 +396,6 @@ __device__ void mod_inverse_p(uint64_t *result, const uint64_t *a_normal) {
                 r[t] = (temp_q[t] & mask) | (rt & inv_mask);
             }
 
-            int32_t temp_delta = delta;
-            int32_t neg = -temp_delta;
             if (swap_flag) {
               delta = -delta;
             }
