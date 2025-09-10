3ca7d3cd148fb1d4975f7d291cc6545485361756
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-09-10 12:04:04 -0300

    secp256k1.cu
    Signed-off-by: lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>

--- secp256k1.cu
+++ secp256k1.cu
@@ -571,7 +571,7 @@ __device__ void jacobian_to_affine(ECPoint *aff, const ECPointJacobian *jac) {
 
     from_montgomery_p(z_norm, jac->Z);
 
-    mod_inverse_p(z_inv, z_norm);
+    //mod_inverse_p(z_inv, z_norm);
     mod_mul_mont_p(z_inv_sqr, z_inv, z_inv);
     mod_mul_mont_p(z_inv_cube, z_inv_sqr, z_inv);
     mod_mul_mont_p(aff->x, jac->X, z_inv_sqr);
@@ -895,6 +895,7 @@ int main() {
     cudaFree(result_device);
     return 0;
 }
+
 
 
 
