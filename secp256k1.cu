565b38ca0f50cd714c172285c0802d770fecbd10
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-09-13 18:53:02 -0300

    secp256k1.cu

--- secp256k1.cu
+++ secp256k1.cu
@@ -1305,45 +1305,6 @@ __global__ void get_compressed_public_key(unsigned char *out, const ECPoint *pub
     kernel_get_compressed_public_key(out, pub);
 }
 
-/*
-__global__ void test_mod_inverse(__uint256_t f, __uint256_t g, __uint256_t* result) {
-    *result = recip2(f, g);
-}
-*/
-
-/*
-int main() {
-
-    // f = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
-    __uint256_t f = {};
-    f.limb[0] = (((__uint128_t)0xFFFFFFFFFFFFFFFFULL) << 64) | (__uint128_t)0xFFFFFFFFFFFFFFFFULL;
-    f.limb[1] = (((__uint128_t)0xFFFFFFFFFFFFFFFFULL) << 64) | (__uint128_t)0xFFFFFFFEFFFFFC2FULL;
-
-    // g = 0x33e7665705359f04f28b88cf897c603c9
-    __uint256_t g = {};
-    g.limb[0] = (__uint128_t)0x33e7665705359f04ULL;
-    g.limb[1] = ((__uint128_t)0xf << 64) | (__uint128_t)0x28b88cf897c603c9ULL;
-
-    // g ≡ 1 (mod f):
-    //Hex: 7FDB62ED2D6FA0874ABD664C95B7CEF2ED79CC82D13FF3AC8E9766AA21BEBEAE (bebeae kkk)
-    //Dec: 57831354042695616917422878622316954017183908093256327737334808907053491207854
-
-    __uint256_t result_host;
-    __uint256_t* result_device;
-    cudaMalloc(&result_device, sizeof(__uint256_t));
-
-    test_mod_inverse<<<1,1>>>(f, g, result_device);
-
-    cudaMemcpy(&result_host, result_device, sizeof(__uint256_t), cudaMemcpyDeviceToHost);
-
-    printf("Resultado limb[1] (alto): %llx\n", (unsigned long long)result_host.limb[1]);
-    printf("Resultado limb[0] (baixo): %llx\n", (unsigned long long)result_host.limb[0]);
-
-    cudaFree(result_device);
-    return 0;
-}
-*/
-
 __global__ void test_mod_inverse(const __uint256_t* f, const __uint256_t* g, __uint256_t* result) {
     *result = recip2(*f, *g);
 }
