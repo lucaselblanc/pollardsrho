4e66f5f6c1db6966cbad86eac99232ad504b9de0
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-09-21 17:13:57 -0300

    secp256k1.cu

--- secp256k1.cu
+++ secp256k1.cu
@@ -783,8 +783,9 @@ int main() {
     const int TARGET_NUM_KERNELS = 32;
     int ITER_PER_KERNEL = (TOTAL_ITER + TARGET_NUM_KERNELS - 1) / TARGET_NUM_KERNELS;
     int total_threads = THREADS * BLOCKS;
-    int ITER_PER_THREAD = (ITER_PER_KERNEL + total_threads - 1) / total_threads;
-    int num_kernels = (TOTAL_ITER + ITER_PER_THREAD*total_threads - 1) / (ITER_PER_THREAD*total_threads);
+    int ITER_PER_THREAD = (TOTAL_ITER + total_threads - 1) / total_threads;
+    int ITER_PER_KERNEL = ITER_PER_THREAD * total_threads;
+    int num_kernels = (TOTAL_ITER + ITER_PER_KERNEL - 1) / ITER_PER_KERNEL;
 
     std::cout << "Threads: " << THREADS << ", Blocks: " << BLOCKS << std::endl;
     std::cout << "ITER_PER_THREAD: " << ITER_PER_THREAD << std::endl;
