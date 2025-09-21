d759e656e9b0a33fddac01a644a688dd627811da
lucasleblanc <136483947+lucaselblanc@users.noreply.github.com>
2025-09-21 12:16:45 -0300

    secp256k1.cu

--- secp256k1.cu
+++ secp256k1.cu
@@ -786,7 +786,10 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
     auto last_report = start;
 
-    keygen_kernel<<<BLOCKS, THREADS>>>(d_priv_keys, d_counter, ITER_PER_THREAD);
+    for(int k = 0; k < num_kernels; k++)
+    {
+       keygen_kernel<<<BLOCKS, THREADS>>>(d_priv_keys, d_counter, ITER_PER_THREAD);
+    }
 
     for(int i = 0; i < TOTAL_ITER; i++)
     {
